daad185c4a3ad595ccaaac39e150dfa2c1a9446f
ukasz09 <246703@student.pwr.edu.pl>
2021-03-06 09:30:58 +0100

    Init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'pcb-ui';
+  // rowsQty=16;
+  // colsQty=16;
+  size = 16;
+  test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 }
